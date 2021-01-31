14d98a98489f9eebec8a45800d0a27e4acc7db79
jhs512 <jangka512@gmail.com>
2021-01-31 09:15:34 +0900

    게시물 컨트롤러에서 입력데이터 유효성체크 추가 및 버그수정

--- src/main/java/com/sbs/untactTeacher/controller/UsrArticleController.java
+++ src/main/java/com/sbs/untactTeacher/controller/UsrArticleController.java
@@ -1,8 +1,6 @@
 package com.sbs.untactTeacher.controller;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,7 +10,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.sbs.untactTeacher.dto.Article;
 import com.sbs.untactTeacher.dto.ResultData;
 import com.sbs.untactTeacher.service.ArticleService;
-import com.sbs.untactTeacher.util.Util;
 
 @Controller
 public class UsrArticleController {
@@ -36,11 +33,11 @@ public class UsrArticleController {
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public ResultData doAdd(String title, String body) {
-		if (title != null) {
+		if (title == null) {
 			return new ResultData("F-1", "title을 입력해주세요.");
 		}
 
-		if (body != null) {
+		if (body == null) {
 			return new ResultData("F-1", "body를 입력해주세요.");
 		}
 
@@ -49,7 +46,11 @@ public class UsrArticleController {
 
 	@RequestMapping("/usr/article/doDelete")
 	@ResponseBody
-	public ResultData doDelete(int id) {
+	public ResultData doDelete(Integer id) {
+		if (id == null) {
+			return new ResultData("F-1", "id를 입력해주세요.");
+		}
+
 		Article article = articleService.getArticle(id);
 
 		if (article == null) {
@@ -61,7 +62,19 @@ public class UsrArticleController {
 
 	@RequestMapping("/usr/article/doModify")
 	@ResponseBody
-	public ResultData doModify(int id, String title, String body) {
+	public ResultData doModify(Integer id, String title, String body) {
+		if (id == null) {
+			return new ResultData("F-1", "id를 입력해주세요.");
+		}
+
+		if (title == null) {
+			return new ResultData("F-1", "title을 입력해주세요.");
+		}
+
+		if (body == null) {
+			return new ResultData("F-1", "body를 입력해주세요.");
+		}
+
 		Article article = articleService.getArticle(id);
 
 		if (article == null) {
