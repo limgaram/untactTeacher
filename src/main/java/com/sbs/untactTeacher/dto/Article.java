cf7a8efef8831c6ca1490f06ea4529c03c5900ef
jhs512 <jangka512@gmail.com>
2021-02-20 11:12:24 +0900

    게시판 별 리스팅

--- src/main/java/com/sbs/untactTeacher/dto/Article.java
+++ src/main/java/com/sbs/untactTeacher/dto/Article.java
@@ -11,9 +11,10 @@ public class Article {
 	private int id;
 	private String regDate;
 	private String updateDate;
+	private int boardId;
 	private int memberId;
 	private String title;
 	private String body;
-	
+
 	private String extra__writer;
 }
